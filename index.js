466948ad6dc79a5ecc42c7e126c106b22c92e5b5
unknown <guella.roxane@gmail.com>
2021-03-09 13:35:12 +0100

    add public repertory

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const app = require('express')();
+const express = require('express');
+const app=express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
@@ -6,19 +7,34 @@ var md5 = require('md5');
 let names=[];
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/public/index.html');
 });
+app.use(express.static('public'));
 
 io.on('connection', (socket) => {
-    socket.on('online', connected => {
-        console.log(names);
-        
-        io.emit('onligne', names);
-    });
+    socket.on('co',isSet=>{
+        io.emit('co',names);
+
+    })
+
+    socket.on('write',nom=>{
+        io.emit('write',nom);
+
+    })
 
     socket.on('userName', name => {
         name.image='https://www.gravatar.com/avatar/'+md5(name.email.toLowerCase());
-        names.push(name);
+        let bon=1
+        for (let i=0;i<names.length;i++){
+            if(name.email===names[i].email){
+                bon=0
+                names[i].nomUser=name.nomUser
+            }
+        }
+        console.log(bon)
+        if(bon===1){
+            names.push(name);
+        }
         console.log(names);
         io.emit('userName', name);
         socket.on('disconnect', () => {
